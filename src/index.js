30585da2e27e1ad7a4dcead0d2ea8e349dfbad87
jbettmann <jordan.bettmann@gmail.com>
2022-09-29 20:00:17 -0600

    updated

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/" className="App">
+    <BrowserRouter>
       <MainView />
     </BrowserRouter>
   </React.StrictMode>
