28ac7997198293c67fff016367754d35278cd19c
jbettmann <jordan.bettmann@gmail.com>
2022-09-30 08:44:20 -0600

    bug fix

--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import { MainView } from "./components/main-view/main-view";
 import { BrowserRouter } from "react-router-dom";
+import { Login } from "./components/login/login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <MainView />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+root.render(<Login />);
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <MainView />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
