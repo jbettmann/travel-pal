94ecba6f619785391ad7d1e342ccacf7320b073a
jbettmann <jordan.bettmann@gmail.com>
2022-09-29 19:12:32 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.scss";
+
 import { MainView } from "./components/main-view/main-view";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter className="App">
+    <BrowserRouter basename="/index.html" className="App">
       <MainView />
     </BrowserRouter>
   );
